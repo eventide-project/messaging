d5a5ab7e08064587e1de8001c1f6679115daa9f6
Scott Bellware <sbellware@gmail.com>
2016-10-31 19:15:19 -0500

    Sketch of write test

--- test/automated/write/_write.rb
+++ test/automated/write/_write.rb
@@ -6,33 +6,16 @@ context "Write" do
 
   written_position = Write.(message, stream.name)
 
+  ## TODO
+  ## Move read to EventSource root lib
+  ## Create Postgres Read to do read
+
   read_message = nil
   EventSource::Postgres::Read.(stream, position: written_position, batch_size: 1) do |event_data|
-    read_message = Transform::Read.(event_data, :event_data, message.class)
+    read_message = Message::Import.(event_data, message.class)
   end
-
 
   test "Got the event that was written" do
     assert(read_message == message)
-  end
-end
-
-require_relative './client_integration_init'
-
-context "Message Writer" do
-  test "Writes a message" do
-    message = EventStore::Messaging::Controls::Message.example
-
-    writer = EventStore::Messaging::Writer.build
-
-    stream_name = EventStore::Messaging::Controls::StreamName.get 'testWriter'
-
-    writer.write message, stream_name
-
-    path = "/streams/#{stream_name}"
-    get = EventStore::Client::HTTP::Request::Get.build
-    body_text, get_response = get.("#{path}/0")
-
-    assert(!body_text.nil?)
   end
 end
