735f3986fad02d139e8b1db602ec00bfc53e3b2c
Scott Bellware <sbellware@gmail.com>
2016-11-01 20:50:48 -0500

    Sketch of writer (realized needs decoupling from postgres)

--- lib/messaging/write.rb
+++ lib/messaging/write.rb
@@ -1,4 +1,29 @@
 module Messaging
   class Write
+    include Log::Dependency
+
+    dependency :event_writer
+
+    def self.build(partition: nil, session: nil)
+      instance = new
+      instance.configure(partition: partition, session: session)
+      instance
+    end
+
+    def self.configure(receiver, partition: nil, session: nil, attr_name: nil)
+      attr_name ||= :writer
+      instance = build(partition: partition, session: session)
+      receiver.public_send "#{attr_name}=", instance
+    end
+
+    def configure(partition: nil, session: nil)
+      EventSource::Postres::Write.configure(self, attr_name: event_writer, partition: partition, session: session)
+    end
+
+    def self.call(message, stream_name, partition: nil, session: nil)
+    end
+
+    def call(message)
+    end
   end
 end
--- test/automated/write/_write.rb
+++ test/automated/write/_write.rb
@@ -2,16 +2,16 @@ require_relative '../automated_init'
 
 context "Write" do
   stream = Controls::Stream.example
+
   message = Controls::Message.example
 
   written_position = Write.(message, stream.name)
 
-  ## TODO
-  ## Move read to EventSource root lib
-  ## Create Postgres Read to do read
+  ## Can't bind to postgres here, can we?
+  ## Need to separate
 
   read_message = nil
-  EventSource::Postgres::Read.(stream, position: written_position, batch_size: 1) do |event_data|
+  EventSource::Postgres::Read.(stream.name, position: written_position, batch_size: 1) do |event_data|
     read_message = Message::Import.(event_data, message.class)
   end
 
