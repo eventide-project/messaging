dd45009a7fd8277ded5dc9cae865e9c7fa24a3d9
Scott Bellware <sbellware@gmail.com>
2016-11-04 00:36:22 -0500

    Expected version test

---
+++ test/automated/write/expected_version.rb
@@ -0,0 +1,27 @@
+require_relative '../automated_init'
+
+context "Write" do
+  context "Single Message" do
+    stream_name = Controls::StreamName.example(category: 'testWrongVersion')
+
+    message_1 = Controls::Message.example
+
+    Write.(message_1, stream_name)
+
+    message_2 = Controls::Message.example
+
+    context "Right Version" do
+      test "Succeeds" do
+        Write.(message_2, stream_name, expected_version: 0)
+      end
+    end
+
+    context "Wrong Version" do
+      test "Fails" do
+        assert proc { Write.(message_2, stream_name, expected_version: 11) } do
+          raises_error? EventSource::ExpectedVersionError
+        end
+      end
+    end
+  end
+end
