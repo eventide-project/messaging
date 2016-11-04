f69000cf9d8ea726b64e7ba14b126a1744ea121a
Scott Bellware <sbellware@gmail.com>
2016-11-03 23:19:44 -0500

    Reply and reply stream name

--- lib/messaging/write.rb
+++ lib/messaging/write.rb
@@ -37,15 +37,40 @@ module Messaging
       end
     end
 
-    def call(message, stream_name, expected_version: nil)
-      logger.trace { "Writing message (Stream Name: #{stream_name}, Type: #{message.class.message_type}, Expected Version: #{expected_version.inspect})" }
+    def call(message, stream_name, expected_version: nil, reply_stream_name: nil)
+      logger.trace { "Writing message (Stream Name: #{stream_name}, Type: #{message.class.message_type}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect})" }
       logger.trace(tags: [:data, :message]) { message.pretty_inspect }
+
+      if reply_stream_name
+         message.metadata.reply_stream_name = reply_stream_name
+      end
 
       event_data = Message::Export.(message)
 
       event_writer.(event_data, stream_name, expected_version: expected_version).tap do
-        logger.debug { "Wrote message (Stream Name: #{stream_name}, Type: #{message.class.message_type}, Expected Version: #{expected_version.inspect})" }
+        logger.debug { "Wrote message (Stream Name: #{stream_name}, Type: #{message.class.message_type}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect})" }
         logger.debug(tags: [:data, :message]) { message.pretty_inspect }
+      end
+    end
+    alias :write :call
+
+    def reply(message)
+      metadata = message.metadata
+      reply_stream_name = metadata.reply_stream_name
+
+      logger.trace { "Replying (Message Type: #{message.message_type}, Stream Name: #{reply_stream_name})" }
+
+      unless reply_stream_name
+        error_msg = "Message has no reply stream name. Cannot reply. (Message Type: #{message.message_type})"
+        logger.error { error_msg }
+        raise Error, error_msg
+      end
+
+      metadata.clear_reply_stream_name
+
+      write(message, reply_stream_name).tap do
+        logger.info { "Replied (Message Type: #{message.message_type}, Stream Name: #{reply_stream_name})" }
+        # telemetry.record :replied, Telemetry::Data.new(message, reply_stream_name)
       end
     end
   end
