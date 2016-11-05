9d4cd07998a32a11c853f6c14263bc95e0dddcc4
Scott Bellware <sbellware@gmail.com>
2016-11-05 18:32:14 -0500

    Tests for initial write of batch

---
+++ test/automated/write/batch/write_initial.rb
@@ -0,0 +1,48 @@
+require_relative '../../automated_init'
+
+context "Write" do
+  context "Message" do
+    context "Writing the initial event to a stream that has not been created yet" do
+      stream_name = Controls::StreamName.example(category: 'testBatchWriteInitial')
+
+      message_1 = Controls::Message.example(some_attribute: 'value_1')
+      message_2 = Controls::Message.example(some_attribute: 'value_2')
+
+      batch = [message_1, message_2]
+
+      writer = Messaging::Postgres::Write.build
+
+      writer.write_initial(batch, stream_name)
+
+      context "Individual Events are Written" do
+        2.times do |i|
+          read_event = EventSource::Postgres::Get.(stream_name, position: i, batch_size: 1).first
+
+          test "Event #{i + 1}" do
+            assert(read_event.data[:some_attribute] == "value_#{i + 1}")
+          end
+        end
+      end
+    end
+
+    context "Writing the initial event to a stream that already exists" do
+      stream_name = Controls::StreamName.example
+
+      message_1 = Controls::Message.example
+      message_2 = Controls::Message.example
+
+      batch = [message_1, message_2]
+
+      writer = Messaging::Postgres::Write.build
+
+      message = Controls::Message.example
+      Write.(message, stream_name)
+
+      test "Is an error" do
+        assert proc { writer.write_initial(batch, stream_name) } do
+          raises_error? EventSource::ExpectedVersion::Error
+        end
+      end
+    end
+  end
+end
--- test/automated/write/message/write_initial.rb
+++ test/automated/write/message/write_initial.rb
@@ -3,7 +3,7 @@ require_relative '../../automated_init'
 context "Write" do
   context "Message" do
     context "Writing the initial event to a stream that has not been created yet" do
-      stream_name = Controls::StreamName.example
+      stream_name = Controls::StreamName.example(category: 'testMessageWriteInitial')
 
       message = Controls::Message.example
 
@@ -31,37 +31,6 @@ context "Write" do
         assert proc { writer.write_initial(message, stream_name) } do
           raises_error? EventSource::ExpectedVersion::Error
         end
-      end
-    end
-  end
-end
-__END__
-
-context "Initial Event" do
-  context "Writing the initial event to a stream that has not been created yet" do
-    substitute_writer = EventStore::Messaging::Writer::Substitute.build
-
-    stream_name = EventStore::Messaging::Controls::StreamName.get 'testWriter'
-    message = EventStore::Messaging::Controls::Message.example
-
-    substitute_writer.write_initial message, stream_name
-
-    test "Has an expected version of :no_stream" do
-      assert(substitute_writer.written? { |msg, stream, expected_version | message == msg && expected_version == :no_stream })
-    end
-  end
-
-  context "Writing the initial event to a stream that was previously created by the writing of another event" do
-    writer = EventStore::Messaging::Writer.build
-
-    message = EventStore::Messaging::Controls::Message.example
-    stream_name = EventStore::Messaging::Controls::StreamName.get 'testWriter'
-
-    writer.write message, stream_name
-
-    test "Is an error" do
-      assert proc { writer.write_initial message, stream_name } do
-        raises_error? EventStore::Client::HTTP::Request::Post::ExpectedVersionError
       end
     end
   end
