27103f9f0077a2bd86d2bf60e79294af230ba859
Scott Bellware <sbellware@gmail.com>
2016-11-05 01:23:26 -0500

    Write initial

--- test/automated/write/message/write_initial.rb
+++ test/automated/write/message/write_initial.rb
@@ -2,19 +2,35 @@ require_relative '../../automated_init'
 
 context "Write" do
   context "Message" do
-    context "Initial" do
-      random_id = SecureRandom.hex
-      stream_name = Controls::StreamName.example(id: random_id)
+    context "Writing the initial event to a stream that has not been created yet" do
+      stream_name = Controls::StreamName.example
 
       message = Controls::Message.example
 
       writer = Messaging::Postgres::Write.build
+
       writer.write_initial(message, stream_name)
 
       read_event = EventSource::Postgres::Get.(stream_name, position: 0, batch_size: 1).first
 
       test "Writes the message" do
         assert(read_event.data == message.to_h)
+      end
+    end
+
+    context "Writing the initial event to a stream that already exists" do
+      stream_name = Controls::StreamName.example
+
+      message = Controls::Message.example
+
+      writer = Messaging::Postgres::Write.build
+
+      writer.write(message, stream_name)
+
+      test "Is an error" do
+        assert proc { writer.write_initial(message, stream_name) } do
+          raises_error? EventSource::ExpectedVersion::Error
+        end
       end
     end
   end
