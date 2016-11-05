c0181def826a0964b61f4992b833df53a3dee43c
Scott Bellware <sbellware@gmail.com>
2016-11-04 19:33:46 -0500

    Test for reply checks that the reply stream name is cleared from the metadata

---
+++ test/automated/write/message/reply.rb
@@ -0,0 +1,26 @@
+require_relative '../../automated_init'
+
+context "Write" do
+  context "Message" do
+    context "Replying to a Message" do
+      message = Controls::Message.example
+
+      reply_stream_name = Controls::StreamName.example(category: 'replyToTestReply')
+      message.metadata.reply_stream_name = reply_stream_name
+
+      writer = Messaging::Postgres::Write.build
+
+      written_position = writer.reply(message)
+
+      read_event = EventSource::Postgres::Get.(reply_stream_name, position: written_position, batch_size: 1).first
+
+      test "Writes the message to the reply stream" do
+        assert(read_event.data == message.to_h)
+      end
+
+      test "Clears the reply stream from the metadata" do
+        assert(read_event.metadata[:reply_stream_name].nil?)
+      end
+    end
+  end
+end
