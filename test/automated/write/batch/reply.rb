5cdc365a70a3750c071ebb3c3a6be600515b418e
Scott Bellware <sbellware@gmail.com>
2016-11-06 14:54:52 -0600

    Test for reply with batch error

---
+++ test/automated/write/batch/reply.rb
@@ -0,0 +1,22 @@
+require_relative '../../automated_init'
+
+context "Write" do
+  context "Reply" do
+    context "Batch" do
+      reply_stream_name = Controls::StreamName.example(category: 'testReplyToBatchError')
+
+      message_1 = Controls::Message.example
+      message_2 = Controls::Message.example
+
+      batch = [message_1, message_2]
+
+      writer = Controls::Write.example
+
+      test "Is an error" do
+        assert proc { writer.reply(batch) } do
+          raises_error? Messaging::Write::Error
+        end
+      end
+    end
+  end
+end
