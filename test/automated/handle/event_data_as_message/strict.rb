fd41cc1650e4b855fa188e50c56e1710c338d566
Scott Bellware <sbellware@gmail.com>
2016-11-07 23:18:17 -0600

    Handle event data as message, strict

---
+++ test/automated/handle/event_data_as_message/strict.rb
@@ -0,0 +1,55 @@
+require_relative '../../automated_init'
+
+context "Handle" do
+  context "Message" do
+    context "Strict" do
+      context "Handler Implements Message Handler for EventData's Type" do
+        context "Message Handler" do
+          event_data = Controls::EventData::Read.example(type: 'SomeMessage')
+
+          message = Controls::Handler::Message::Example.(event_data, strict: true)
+
+          test "EventData is handled as Message" do
+            assert(message.some_attribute == 'some value set by handler')
+          end
+        end
+
+        context "EventData Handler" do
+          event_data = Controls::EventData::Read.example(type: 'SomeMessage')
+
+          unchanged_data = event_data.data
+
+          message = Controls::Handler::MessageAndEventData::Example.(event_data, strict: true)
+
+          message_data = message.to_h
+
+          test "EventData is not handled as EventData" do
+            refute(event_data.data == message_data )
+          end
+        end
+      end
+
+      context "Handler Does not Implement Message Handler for EventData's Type" do
+        context "Handler Implements Handle" do
+          event_data = Controls::EventData::Read.example(type: 'SomeMessage')
+
+          Controls::Handler::EventData::Example.(event_data, strict: true)
+
+          test "EventData is handled as EventData" do
+            assert(event_data.data == 'some value set by handler')
+          end
+        end
+
+        context "Handler Does not Implement Handle" do
+          event_data = Controls::EventData::Read.example(type: 'SomeMessage')
+
+          test "Is an error" do
+            assert proc { Controls::Handler::NoHandle::Example.(event_data, strict: true) } do
+              raises_error? Handle::Error
+            end
+          end
+        end
+      end
+    end
+  end
+end
