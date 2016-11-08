d398c6a29ac37556aa79a09137d2f13714f4e6cc
Scott Bellware <sbellware@gmail.com>
2016-11-08 16:56:50 -0600

    Superfluous test coverage is removed

--- test/automated/handle/event_data/strict.rb
+++ test/automated/handle/event_data/strict.rb
@@ -3,16 +3,6 @@ require_relative '../../automated_init'
 context "Handle" do
   context "EventData" do
     context "Strict" do
-      context "Handler Implements Handle" do
-        event_data = Controls::EventData::Read.example(data: 1)
-
-        Controls::Handler::EventData::Example.(event_data, strict: true)
-
-        test "Event data is handled" do
-          assert(event_data.data == 'some value set by handler')
-        end
-      end
-
       context "Handler Does Not Implement Handle" do
         event_data = Controls::EventData::Read.example(data: 1)
 
--- test/automated/handle/message/strict.rb
+++ test/automated/handle/message/strict.rb
@@ -3,16 +3,6 @@ require_relative '../../automated_init'
 context "Handle" do
   context "Message" do
     context "Strict" do
-      context "Handler Implements Handler for Message" do
-        message = Controls::Message::New.example
-
-        Controls::Handler::Message::Example.(message, strict: true)
-
-        test "Message is handled" do
-          assert(message.some_attribute == 'some value set by handler')
-        end
-      end
-
       context "Handler Does Not Implement Handle" do
         message = Controls::Message::New.example
 
