dcee7d7ccf040bf7ff40aff42966a60ca7cb8a3b
Scott Bellware <sbellware@gmail.com>
2016-11-18 17:32:42 -0600

    Partition is an instance actuator parameter rather than a class constructor parameter

--- lib/messaging/write.rb
+++ lib/messaging/write.rb
@@ -18,46 +18,46 @@ module Messaging
     end
 
     module Build
-      def build(partition: nil, session: nil)
+      def build(session: nil)
         instance = new
-        instance.configure(partition: partition, session: session)
+        instance.configure(session: session)
         ::Telemetry.configure instance
         instance
       end
     end
 
     module Configure
-      def configure(receiver, partition: nil, session: nil, attr_name: nil)
+      def configure(receiver, session: nil, attr_name: nil)
         attr_name ||= :writer
-        instance = build(partition: partition, session: session)
+        instance = build(session: session)
         receiver.public_send "#{attr_name}=", instance
       end
     end
 
     module Call
       def call(message, stream_name, expected_version: nil, reply_stream_name: nil, partition: nil, session: nil)
-        instance = build(partition: partition, session: session)
-        instance.(message, stream_name, expected_version: expected_version, reply_stream_name: reply_stream_name)
+        instance = build(session: session)
+        instance.(message, stream_name, expected_version: expected_version, reply_stream_name: reply_stream_name, partition: partition)
       end
     end
 
-    def call(message_or_batch, stream_name, expected_version: nil, reply_stream_name: nil)
+    def call(message_or_batch, stream_name, expected_version: nil, reply_stream_name: nil, partition: nil)
       unless message_or_batch.is_a? Array
-        logger.trace { "Writing message_or_batch (Stream Name: #{stream_name}, Type: #{message_or_batch.class.message_type}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect})" }
+        logger.trace { "Writing message_or_batch (Stream Name: #{stream_name}, Type: #{message_or_batch.class.message_type}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect}, Partition: #{partition.inspect})" }
       else
-        logger.trace { "Writing batch (Stream Name: #{stream_name}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect})" }
+        logger.trace { "Writing batch (Stream Name: #{stream_name}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect}, Partition: #{partition.inspect})" }
       end
       logger.trace(tags: [:data, :message]) { message_or_batch.pretty_inspect }
 
       message_batch = Array(message_or_batch)
 
       event_data_batch = event_data_batch(message_batch, reply_stream_name)
-      last_position = event_writer.(event_data_batch, stream_name, expected_version: expected_version)
+      last_position = event_writer.(event_data_batch, stream_name, expected_version: expected_version, partition: partition)
 
       unless message_or_batch.is_a? Array
-        logger.info { "Wrote message_or_batch (Position: #{last_position}, Stream Name: #{stream_name}, Type: #{message_or_batch.class.message_type}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect})" }
+        logger.info { "Wrote message_or_batch (Position: #{last_position}, Stream Name: #{stream_name}, Type: #{message_or_batch.class.message_type}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect}, Partition: #{partition.inspect})" }
       else
-        logger.info { "Wrote batch (Position: #{last_position}, Stream Name: #{stream_name}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect})" }
+        logger.info { "Wrote batch (Position: #{last_position}, Stream Name: #{stream_name}, Expected Version: #{expected_version.inspect}, Reply Stream Name #{reply_stream_name.inspect}, Partition: #{partition.inspect})" }
       end
       logger.info(tags: [:data, :message]) { event_data_batch.pretty_inspect }
 
@@ -82,7 +82,7 @@ module Messaging
       event_data_batch
     end
 
-    def reply(message)
+    def reply(message, partition: nil)
       if message.is_a? Array
         error_msg = "Cannot reply with a batch"
         logger.error { error_msg }
@@ -92,24 +92,25 @@ module Messaging
       metadata = message.metadata
       reply_stream_name = metadata.reply_stream_name
 
-      logger.trace { "Replying (Message Type: #{message.message_type}, Stream Name: #{reply_stream_name})" }
+      logger.trace { "Replying (Message Type: #{message.message_type}, Stream Name: #{reply_stream_name.inspect}, Partition: #{partition.inspect})" }
 
       if reply_stream_name.nil?
-        error_msg = "Message has no reply stream name. Cannot reply. (Message Type: #{message.message_type})"
+        error_msg = "Message has no reply stream name. Cannot reply. (Message Type: #{message.message_type}, Partition: #{partition.inspect})"
         logger.error { error_msg }
+        logger.error(tags: [:data, :message]) { message.pretty_inspect }
         raise Error, error_msg
       end
 
       metadata.clear_reply_stream_name
 
-      write(message, reply_stream_name).tap do
-        logger.info { "Replied (Message Type: #{message.message_type}, Stream Name: #{reply_stream_name})" }
+      write(message, reply_stream_name, partition: partition).tap do
+        logger.info { "Replied (Message Type: #{message.message_type}, Stream Name: #{reply_stream_name}, Partition: #{partition.inspect})" }
         telemetry.record :replied, Telemetry::Data.new(message, reply_stream_name)
       end
     end
 
-    def write_initial(message, stream_name)
-      write(message, stream_name, expected_version: :no_stream)
+    def write_initial(message, stream_name, partition: nil)
+      write(message, stream_name, expected_version: :no_stream, partition: partition)
     end
 
     def self.register_telemetry_sink(writer)
