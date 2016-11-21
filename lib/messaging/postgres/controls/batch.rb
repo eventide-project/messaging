1484697ce08967696c3f51ff1d6a8adbe82f054b
Scott Bellware <sbellware@gmail.com>
2016-11-20 20:20:31 -0600

    Batch control

--- lib/messaging/postgres/controls/batch.rb
+++ lib/messaging/postgres/controls/batch.rb
@@ -1,31 +1,7 @@
 module Messaging
   module Postgres
     module Controls
-      module Batch
-        def self.example
-          values = [
-            EventSource::Controls::RandomValue.example,
-            EventSource::Controls::RandomValue.example
-          ]
-
-          batch = Messages.example
-
-          2.times do |i|
-            batch[i].some_attribute = values[i]
-          end
-
-          return batch, values
-        end
-
-        module Messages
-          def self.example
-            [
-              Controls::Message.example,
-              Controls::Message.example
-            ]
-          end
-        end
-      end
+      Batch = Messaging::Controls::Batch
     end
   end
 end
