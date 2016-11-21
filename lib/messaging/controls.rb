002d9566710c320e50be05478076035cb6d72df5
Scott Bellware <sbellware@gmail.com>
2016-11-20 20:16:47 -0600

    Batch control

--- lib/messaging/controls.rb
+++ lib/messaging/controls.rb
@@ -2,14 +2,11 @@ require 'clock/controls'
 require 'event_source/controls'
 
 require 'messaging/controls/time'
-require 'messaging/controls/message'
-require 'messaging/controls/metadata'
-
-require 'messaging/controls/event_data'
-
 require 'messaging/controls/stream'
 require 'messaging/controls/stream_name'
-
+require 'messaging/controls/event_data'
+require 'messaging/controls/message'
+require 'messaging/controls/metadata'
+require 'messaging/controls/batch'
 require 'messaging/controls/write'
-
 require 'messaging/controls/handler'
---
+++ lib/messaging/controls/batch.rb
@@ -0,0 +1,29 @@
+module Messaging
+  module Controls
+    module Batch
+      def self.example
+        values = [
+          EventSource::Controls::RandomValue.example,
+          EventSource::Controls::RandomValue.example
+        ]
+
+        batch = Messages.example
+
+        2.times do |i|
+          batch[i].some_attribute = values[i]
+        end
+
+        return batch, values
+      end
+
+      module Messages
+        def self.example
+          [
+            Controls::Message.example,
+            Controls::Message.example
+          ]
+        end
+      end
+    end
+  end
+end
