c2467c4aa995def29d09bf26541a232c16b2f4a9
Scott Bellware <sbellware@gmail.com>
2016-12-21 15:55:18 -0600

    Interactive test's consumer also measures throughput

--- test/interactive/consume.rb
+++ test/interactive/consume.rb
@@ -3,6 +3,8 @@ require_relative 'controls'
 
 logger = Log.get('Consume')
 
+logger.level = :info
+
 logger.info "Starting Consumer", tag: :test
 
 stream_name_file = File.expand_path('stream_name.tmp', File.dirname(__FILE__))
@@ -17,12 +19,26 @@ logger.info "Stream name: #{stream_name}", tag: :test
 
 logger.info "Starting reader", tag: :test
 
+handler = Handler.build
+
+count = 0
+start_time = Time.now
 EventSource::Postgres::Read.(stream_name, batch_size: 1, cycle_delay_milliseconds: 10, cycle_timeout_milliseconds: 2000) do |event_data|
-  logger.debug event_data.pretty_inspect, tags: [:test, :data, :message]
+  logger.debug(tags: [:test, :data, :message]) { event_data.pretty_inspect }
 
-  message = Handler.(event_data)
+  message = handler.(event_data)
+  count += 1
 
-  logger.info "Handled message: #{message.message_type}", tags: [:test, :data, :message]
+  logger.debug(tags: [:test, :data, :message]) { "Handled message: #{message.message_type}" }
 
-  logger.debug message.pretty_inspect, tags: [:test, :data, :message]
+  logger.debug(tags: [:test, :data, :message]) { message.pretty_inspect }
 end
+
+stop_time = Time.now
+
+duration = stop_time - start_time
+throughput = count / duration
+
+logger.info "Messages: #{count}", tags: [:test, :data, :message]
+logger.info "Duration: #{duration}", tags: [:test, :data, :message]
+logger.info "Throughput: #{throughput}", tags: [:test, :data, :message]
