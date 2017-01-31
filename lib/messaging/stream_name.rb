a4f77c04004641f892b00653b949a8df476e5cdc
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-31 17:05:00 -0600

    Category method is nil coalescing

--- lib/messaging/stream_name.rb
+++ lib/messaging/stream_name.rb
@@ -17,7 +17,7 @@ module Messaging
       def category_macro(category)
         category = Casing::Camel.(category, symbol_to_string: true)
         self.send :define_method, :category do
-          @category || category
+          @category ||= category
         end
       end
       alias :category :category_macro
