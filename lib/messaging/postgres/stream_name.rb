4d6759a31d8ab7781ec50aebeb6fe189c327fda5
Scott Bellware <sbellware@gmail.com>
2017-02-04 23:38:00 -0600

    Category macro is moved to generalization

--- lib/messaging/postgres/stream_name.rb
+++ lib/messaging/postgres/stream_name.rb
@@ -4,28 +4,9 @@ module Messaging
       extend self
 
       def self.included(cls)
-        cls.extend Macro
-      end
-
-      def self.activate
-        target_class ||= Object
-        macro_module = Macro
-        return if target_class.is_a? macro_module
-        target_class.extend(macro_module)
-      end
-
-      module Macro
-        def category_macro(category)
-          category = Casing::Camel.(category, symbol_to_string: true)
-          self.send :define_method, :category do
-            @category ||= category
-          end
-
-          self.send :define_method, :category= do |category|
-            @category = category
-          end
+        cls.class_exec do
+          include Messaging::Category
         end
-        alias :category :category_macro
       end
 
       def stream_name(id, category=nil, type: nil)
