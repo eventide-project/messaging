c16ed9a5d53cd1f93832bd6da1e4f03d64990718
Scott Bellware <sbellware@gmail.com>
2017-04-12 17:48:53 -0500

    Parameter name is corrected

--- lib/messaging/message/export.rb
+++ lib/messaging/message/export.rb
@@ -1,8 +1,8 @@
 module Messaging
   module Message
     module Export
-      def self.call(event_data)
-        Transform::Write.(event_data, :event_data)
+      def self.call(message)
+        Transform::Write.(message, :event_data)
       end
     end
   end
