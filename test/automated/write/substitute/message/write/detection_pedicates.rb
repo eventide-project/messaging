18f395fcf0167948fc7a883f1c1afbb8a6b7ed2b
Scott Bellware <sbellware@gmail.com>
2018-10-03 00:14:05 -0500

    Substitute test coverage is improved

--- test/automated/write/substitute/message/write/detection_pedicates.rb
+++ test/automated/write/substitute/message/write/detection_pedicates.rb
@@ -35,21 +35,22 @@ context "Write" do
           end
 
           context "Message Argument" do
-            test "No block argument" do
-              assert(writer.written?(message))
-            end
+            context "Message That Was Written" do
+              test "No block argument" do
+                assert(writer.written?(message))
+              end
 
-            test "Stream name block argument" do
-              # assert(writer.written?(message) { |stream| stream == stream_name })
-              assert(writer.written?(Object.new) { |stream| stream == stream_name })
-            end
+              test "Stream name block argument" do
+                assert(writer.written?(message) { |stream| stream == stream_name })
+              end
 
-            test "Stream name, and expected_version block arguments" do
-              assert(writer.written?(message) { |stream, expected_version| expected_version == 11 })
-            end
+              test "Stream name, and expected_version block arguments" do
+                assert(writer.written?(message) { |stream, expected_version| expected_version == 11 })
+              end
 
-            test "Stream name, expected_version, and reply_stream_name block arguments" do
-              assert(writer.written?(message) { |stream, expected_version, reply_stream_name| reply_stream_name == 'someReplyStreamName' })
+              test "Stream name, expected_version, and reply_stream_name block arguments" do
+                assert(writer.written?(message) { |stream, expected_version, reply_stream_name| reply_stream_name == 'someReplyStreamName' })
+              end
             end
           end
         end
@@ -83,13 +84,11 @@ context "Write" do
 
           context "Message Argument" do
             test "No block argument" do
-              # refute(writer.written?(message))
-              refute(writer.written?(Object.new))
+              refute(writer.written?(message))
             end
 
             test "Stream name block argument" do
-              # refute(writer.written?(message) { |stream| stream == stream_name })
-              refute(writer.written?(Object.new) { |stream| stream == stream_name })
+              refute(writer.written?(message) { |stream| stream == stream_name })
             end
 
             test "Stream name, and expected_version block arguments" do
