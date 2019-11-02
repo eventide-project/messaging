2be960b4cc348e6cd996305245419b6a3590d5ed
Scott Bellware <sbellware@gmail.com>
2019-11-02 18:51:27 -0500

    Get ID test is clarified

--- test/automated/stream_name/get_id.rb
+++ test/automated/stream_name/get_id.rb
@@ -2,18 +2,23 @@ require_relative '../automated_init'
 
 context "Stream Name" do
   context "Get ID" do
-    test "Is the part of a stream name after the first dash" do
-      id = Identifier::UUID.random
-      stream_name = "someStream-#{id}"
+    context "Single ID in the Stream Name" do
+      id = 'some_id'
+      stream_name = "someCategory-#{id}"
 
       stream_id = StreamName.get_id(stream_name)
 
-      assert(stream_id == id)
+      test "ID value is parsed" do
+        assert(stream_id == 'some_id')
+      end
     end
 
-    test "Is nil if there is no ID part in the stream name" do
+    context "No ID in the Stream Name" do
       stream_id = StreamName.get_id('someStream')
-      assert(stream_id.nil?)
+
+      test "ID is nil" do
+        assert(stream_id.nil?)
+      end
     end
   end
 end
