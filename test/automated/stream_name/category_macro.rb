aa3910b85dff172ff599f9a833ac80dba420ea5d
Scott Bellware <sbellware@gmail.com>
2019-11-02 18:35:18 -0500

    Category macro

--- test/automated/stream_name/category_macro.rb
+++ test/automated/stream_name/category_macro.rb
@@ -3,7 +3,7 @@ require_relative '../automated_init'
 context "Stream Name" do
   example = Controls::StreamName::NamedCategory.example
 
-  context "Macro" do
+  context "Category Macro" do
     test "Adds the category getter" do
       assert(example.respond_to? :category)
     end
