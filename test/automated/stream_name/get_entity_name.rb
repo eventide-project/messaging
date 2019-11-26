8cc3fcd3340c8191717cae24967326a898864956
Scott Bellware <sbellware@gmail.com>
2019-11-25 18:16:46 -0600

    Stream name parsing supports getting the entity name

---
+++ test/automated/stream_name/get_entity_name.rb
@@ -0,0 +1,55 @@
+require_relative '../automated_init'
+
+context "Stream Name" do
+  context "Get Entity Name" do
+    context "Stream Name Does Not Contain an ID" do
+      stream_name = 'someStream'
+
+      enity_name = StreamName.get_entity_name(stream_name)
+
+      test "Entity name is parsed" do
+        assert(enity_name == 'someStream')
+      end
+    end
+
+    context "Stream Name Contains an ID" do
+      stream_name = 'someStream-some_id'
+
+      enity_name = StreamName.get_entity_name(stream_name)
+
+      test "Entity name is parsed" do
+        assert(enity_name == 'someStream')
+      end
+    end
+
+    context "Stream Name Contains Type" do
+      stream_name = 'someStream:someType'
+
+      enity_name = StreamName.get_entity_name(stream_name)
+
+      test "Entity name is parsed" do
+        assert(enity_name == 'someStream')
+      end
+    end
+
+    context "Stream Name Contains Types" do
+      stream_name = 'someStream:someType+someOtherType'
+
+      enity_name = StreamName.get_entity_name(stream_name)
+
+      test "Entity name is parsed" do
+        assert(enity_name == 'someStream')
+      end
+    end
+
+    context "Stream Name Contains ID and Types" do
+      stream_name = 'someStream:someType+someOtherType-some_id'
+
+      enity_name = StreamName.get_entity_name(stream_name)
+
+      test "Entity name is parsed" do
+        assert(enity_name == 'someStream')
+      end
+    end
+  end
+end
