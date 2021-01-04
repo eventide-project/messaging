34f284548b8f6de4c34d3a963c70bd96cc58d9d6
Scott Bellware <sbellware@gmail.com>
2021-01-03 23:10:32 -0600

    Transient metadata properties

---
+++ test/automated/message/transform/write/transient_metadata_properties_not_written.rb
@@ -0,0 +1,33 @@
+require_relative '../../../automated_init'
+
+context "Message" do
+  context "Transform" do
+    context "Transient Metadata Properties Not Written"
+  end
+end
+__END__
+      message = Controls::Message.example
+
+      metadata = message.metadata
+
+      metadata.class.attribute_names.each do |metadata_attribute|
+        message.metadata.send("#{metadata_attribute}=", nil)
+      end
+
+      metadata.properties = Array.new
+
+      message_data = Transform::Write.(message, :message_data)
+
+      detail "Transformed MessageData: #{message_data.pretty_inspect}"
+      detail "Message Metadata: #{metadata.pretty_inspect}"
+
+      context "Metadata" do
+        metadata = message_data.metadata
+
+        test "MessageData metadata has no fields" do
+          assert(message_data.metadata.keys.empty?)
+        end
+      end
+    end
+  end
+end
